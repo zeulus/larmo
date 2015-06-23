32401ea098d1fd7af0b351d93a2876a39fe2d57d
Adrian Pietka <adrian@pietka.me>
2015-06-23 23:22:02 +0200

    Structure for API of Larmo Hub

--- hub/public/index.php
+++ hub/public/index.php
@@ -2,43 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+$app = new Silex\Application;
 
-$app = new Application();
-
-$app['debug'] = true;
-
-$app->post('/registerPacket', function (Request $request) use ($app) {
-    $content = $request->getContent();
-    $response = ["status" => "OK"];
-
-    return new Response(json_encode($response));
-});
-
-$app->get('/latestMessages', function () use ($app) {
-    $messages = [];
-
-    return new Response(json_encode($messages));
-});
-
-$app->get('/availableSources', function () use ($app) {
-    $sources = [
-        ["id" => "skype", "label" => "Skype"],
-        ["id" => "irc", "label" => "IRC"],
-        ["id" => "github", "label" => "GitHub"]
-    ];
-
-    return new Response(json_encode($sources));
-});
-
-$app->error(function (\Exception $exception) use ($app) {
-    if ($app['debug']) {
-        return;
-    }
-
-    return $exception->getMessage();
-});
+require_once __DIR__ . '/../app/config.php';
+require_once __DIR__ . '/../api/routes.php';
+require_once __DIR__ . '/../api/error.php';
 
 $app->run();
