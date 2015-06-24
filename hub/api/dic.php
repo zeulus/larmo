7f569e086746dd80578a468e3ce1cefe431d2e23
Adrian Pietka <adrian@pietka.me>
2015-06-24 14:40:18 +0200

    Validation source when packet is register

--- hub/api/dic.php
+++ hub/api/dic.php
@@ -1,5 +1,7 @@
 <?php
 
+use fkooman\Json\Json;
+
 $app['plugins'] = $app->share(function ($app) {
     $pluginAdapter = new \FP\Larmo\Infrastructure\Adapter\FilesystemPluginsAdapter($app['plugins_directory']);
     $pluginCollection = new \FP\Larmo\Domain\Service\PluginsCollection();
@@ -11,4 +13,16 @@ $app['plugins'] = $app->share(function ($app) {
 
 $app['authinfo'] = $app->share(function ($app) {
     return new \FP\Larmo\Infrastructure\Adapter\IniFileAuthInfoProvider($app['authinfo_config']);
+});
+
+$app['json_parse'] = $app->protect(function ($json) {
+    if ($json) {
+        try {
+            return Json::decode($json);
+        } catch (InvalidArgumentException $exception) {
+            return false;
+        }
+    }
+
+    return false;
 });
