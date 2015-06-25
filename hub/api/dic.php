cd815e4da694fc1040dc483f6aff05035ae82a66
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2015-06-25 11:28:10 +0200

    Extended API endpoints

--- hub/api/dic.php
+++ hub/api/dic.php
@@ -20,6 +20,10 @@ $app['messages.factory'] = $app->share(function () {
     return new \FP\Larmo\Infrastructure\Factory\MessageCollection;
 });
 
+$app['filters.service'] = $app->share(function () {
+   return new \FP\Larmo\Domain\Service\FiltersCollection;
+});
+
 $app['authinfo'] = $app->share(function ($app) {
     return new \FP\Larmo\Infrastructure\Adapter\IniFileAuthInfoProvider($app['authinfo_config']);
 });
