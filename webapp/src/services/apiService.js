edf291f5a04f54daa035a392b838c63e7c569bef
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2015-07-03 10:18:06 +0200

    Fixed api's URLs.

--- webapp/src/services/apiService.js
+++ webapp/src/services/apiService.js
@@ -20,7 +20,7 @@ function apiService(AjaxService, config) {
 
         var url = config.api.useMock
             ? "data/getLatestMessages.json"
-            : config.api.url + "/messages";
+            : config.api.url + "/latestMessages";
 
         return AjaxService.get(url, queryString);
     }
@@ -28,7 +28,7 @@ function apiService(AjaxService, config) {
     function getAvailableSources() {
         var url = config.api.useMock
             ? "data/getAvailableSources.json"
-            : config.api.url + "/sources";
+            : config.api.url + "/availableSources";
 
         return AjaxService.get(url, {t : new Date().getTime()});
     }
