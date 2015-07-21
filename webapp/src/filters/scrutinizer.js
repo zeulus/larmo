ef2c86e33c7490ff8622e813f175202f601f67cb
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2015-07-21 14:57:12 +0200

    Changed js files to better syntax

--- webapp/src/filters/scrutinizer.js
+++ webapp/src/filters/scrutinizer.js
@@ -1,23 +1,22 @@
-'use strict';
+(function(){
+    app.filter('scrutinizerUrlToInspection', urlToInspection);
+    app.filter('scrutinizerSetDescription', setDescription);
 
-app.filter('scrutinizerUrlToInspection', urlToInspection);
-app.filter('scrutinizerSetDescription', setDescription);
-
-function urlToInspection() {
-    return function(extras) {
-        return "https://scrutinizer-ci.com/g/" + extras.repository.user + "/" + extras.repository.name + "/inspections/" + extras.id;
+    function urlToInspection() {
+        return function(extras) {
+            return "https://scrutinizer-ci.com/g/" + extras.repository.user + "/" + extras.repository.name + "/inspections/" + extras.id;
+        }
     }
-}
 
-function setDescription() {
-    return function(extras) {
-        var description = [];
+    function setDescription() {
+        return function(extras) {
+            var description = [];
 
-        [
-            {
-                count: extras.results.added_code_elements,
-                text: "code element{{suffix}} {{verb}} added"
-            }, {
+            [
+                {
+                    count: extras.results.added_code_elements,
+                    text: "code element{{suffix}} {{verb}} added"
+                }, {
                 count: extras.results.changed_code_elements,
                 text: "code element{{suffix}} {{verb}} changed"
             }, {
@@ -33,24 +32,26 @@ function setDescription() {
                 count: extras.results.common_issues,
                 text: "issue{{suffix}} unresolved"
             }
-        ].forEach(function(el){
-            if(el.count > 0) {
-                description.push(setTextWithCorrectVerb(el.count, el.text));
-            }
-        });
+            ].forEach(function(el){
+                    if(el.count > 0) {
+                        description.push(setTextWithCorrectVerb(el.count, el.text));
+                    }
+                });
 
-        return description.join(", ");
-    }
-}
-
-function setTextWithCorrectVerb(count, text) {
-    var verb = "was",
-        suffix = "";
-
-    if(count > 1) {
-        verb = "were";
-        suffix = "s";
+            return description.join(", ");
+        }
     }
 
-    return "<strong>" + count + "</strong> " + text.replace("{{suffix}}", suffix).replace("{{verb}}", verb);
-}
+    function setTextWithCorrectVerb(count, text) {
+        var verb = "was",
+            suffix = "";
+
+        if(count > 1) {
+            verb = "were";
+            suffix = "s";
+        }
+
+        return "<strong>" + count + "</strong> " + text.replace("{{suffix}}", suffix).replace("{{verb}}", verb);
+    }
+})();
+
--- webapp/src/services/apiService.js
+++ webapp/src/services/apiService.js
@@ -1,29 +1,25 @@
-"use strict";
+(function() {
+    app.service("APIService", ["AjaxService", "config", function(AjaxService, config) {
+        var self = {
+            getLatestMessages: getLatestMessages,
+            getAvailableSources: getAvailableSources
+        };
 
-app.service("APIService", apiService);
+        return self;
 
-apiService.$inject = ["AjaxService", "config"];
+        function getLatestMessages(filters, limit) {
+            var queryString = angular.extend(filters, {
+                limit: limit,
+                t: new Date().getTime()
+            });
 
-function apiService(AjaxService, config) {
-    var self = {
-        getLatestMessages: getLatestMessages,
-        getAvailableSources: getAvailableSources
-    };
+            var url = config.api.url + "/latestMessages";
+            return AjaxService.get(url, queryString);
+        }
 
-    return self;
-
-    function getLatestMessages(filters, limit) {
-        var queryString = angular.extend(filters, {
-            limit: limit,
-            t: new Date().getTime()
-        });
-
-        var url = config.api.url + "/latestMessages";
-        return AjaxService.get(url, queryString);
-    }
-
-    function getAvailableSources() {
-        var url = config.api.url + "/availableSources";
-        return AjaxService.get(url, {t : new Date().getTime()});
-    }
-}
+        function getAvailableSources() {
+            var url = config.api.url + "/availableSources";
+            return AjaxService.get(url, {t : new Date().getTime()});
+        }
+    }]);
+})();
